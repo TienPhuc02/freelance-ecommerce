c440420f2c19cc6b0d191a800de766080f9855f0
TienPhuc02 <dtp2352002@gmail.com>
2024-04-06 17:24:45 +0700

    update filter product

--- src/pages/QueryParamsProducts/page.filter-product.tsx
+++ src/pages/QueryParamsProducts/page.filter-product.tsx
@@ -1,4 +1,4 @@
-import { useParams, useSearchParams } from "react-router-dom";
+import { useParams } from "react-router-dom";
 import { APIGetAllProduct } from "../../services/api";
 import { useEffect, useState } from "react";
 import { Checkbox, Form, FormProps, GetProp, message } from "antd";
